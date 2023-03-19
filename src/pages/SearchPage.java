71b272ed34222e9d148a0d8bf5087a6afa1cd5b5
LokeshKumar1779 <kumarlokesh57@gmail.com>
2023-03-19 23:20:42 +0530

    changes in searchfile and logintest

--- src/pages/SearchPage.java
+++ src/pages/SearchPage.java
@@ -4,5 +4,7 @@ public class SearchPage {
 
     public void methodInSearchPage(){
         System.out.println("Hello from Search Page");
+        System.out.println("Logged in ...");
+        System.out.println("Logged out");
     }
 }
--- src/tests/LoginTest.java
+++ src/tests/LoginTest.java
@@ -3,5 +3,6 @@ package tests;
 public class LoginTest {
     public static void main(String[] args) {
         System.out.println("Hello from Login Test..");
+        System.out.println("BYE...");
     }
 }
