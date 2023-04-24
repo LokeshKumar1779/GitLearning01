0a2df24db318da7956a94234313f59208be4edb0
Deepika0788 <deeprani.0788@gmail.com>
2023-04-24 12:17:57 +0530

    code updated as per comments

--- src/pages/OrderPage.java
+++ src/pages/OrderPage.java
@@ -1,7 +1,6 @@
 package pages;
 
 public class OrderPage {
-	
 	public void orderMethod() {
 		System.out.println("Order Page..");
 	}
--- src/tests/OrderTest.java
+++ src/tests/OrderTest.java
@@ -1,7 +1,7 @@
 package tests;
 
 public class OrderTest {
-
+	//@Test
 	public void orderTest() {
 		System.out.println("Order Test...");
 	}
