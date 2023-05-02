56c42e43835f108f1d79a03f9678cc8eb60f50cc
LokeshKumar1779 <kumarlokesh57@gmail.com>
2023-05-02 09:29:58 +0530

    Feature 3

--- src/pages/ProductPage.java
+++ src/pages/ProductPage.java
@@ -5,5 +5,6 @@ public class ProductPage {
     public void product_page(){
         System.out.println("Feature 1..");
         System.out.println("Feature 2..");
+        System.out.println("Feature 3..");
     }
 }
