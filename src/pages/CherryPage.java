65d43749964e94d4383164e1d11fc52b10ddf57f
LokeshKumar1779 <kumarlokesh57@gmail.com>
2023-05-03 11:46:11 +0530

    third cherry commit

--- src/pages/CherryPage.java
+++ src/pages/CherryPage.java
@@ -7,4 +7,8 @@ public class CherryPage {
     public static void main(String[] args) {
         System.out.println("Cherry pick first line");
     }
+
+    public void cherryMethod(){
+        System.out.println("Cherry pick first method");
+    }
 }
