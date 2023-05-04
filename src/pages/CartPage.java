a890d3663d9061382b58c7363ced84b57dc8d442
LokeshKumar1779 <kumarlokesh57@gmail.com>
2023-05-04 11:38:07 +0530

    more changes in cart page by dev1

--- src/pages/CartPage.java
+++ src/pages/CartPage.java
@@ -4,5 +4,8 @@ public class CartPage {
     public void cart(){
         String nameDT1 = "Dev 1";
         System.out.println("Changes by dev 1");
+        if (nameDT1 == null) {
+            System.out.println("Cart page empty");
+        }
     }
 }
