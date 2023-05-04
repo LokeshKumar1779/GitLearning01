fd9b86963ddf54ddd6724347b7fddaaaf3839142
LokeshKumar1779 <89783109+LokeshKumar1779@users.noreply.github.com>
2023-05-04 12:20:02 +0530

    Create Classic.java

---
+++ src/pages/Classic.java
@@ -0,0 +1,11 @@
+package pages;
+
+public class Classic {
+
+    public void classic(){
+        String name = "Lokesh";
+        int age = 20;
+        System.out.println("name : " +name +","+"age : " +age); 
+    }
+
+}
